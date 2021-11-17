f50fa10e3027ba5faf34b07e62e3faedef71552f
Tim Carr <hello@timmyc.dev>
2021-11-17 00:10:13 +0000

    04 month and year selectors are now working.

--- calendar.js
+++ calendar.js
@@ -8,43 +8,57 @@ export const drawCalendar = (today) => {
   let year = today.getFullYear()
   let date = today.getDate()
   
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const shortdays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
-  let weekday = days[today.getDay()];
+  const longDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const shortDays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  let weekday = longDays[today.getDay()];
   
   // Month and Year in banner
-  document.getElementById("monthWord").appendChild(document.createTextNode(month));
-  document.getElementById("yearNumber").appendChild(document.createTextNode(year));
-  
+    const monthWord = document.getElementById("monthWord");
+    monthWord.innerHTML = "";
+    monthWord.appendChild(document.createTextNode(month));
+    
+    const yearNumber = document.getElementById("yearNumber");
+    yearNumber.innerHTML = "";
+    yearNumber.appendChild(document.createTextNode(year));
+    
   // Weekdays Mo-Su
-  let index;
-  for (index=0;   index<7; index++) {
-    let node = document.createElement("li");
-    // let nbsp = document.createTextNode("\u00A0")
-    node.appendChild(document.createTextNode(shortdays[index]));
-    document.getElementById("weekdays").appendChild(node);
-    // document.getElementById("weekdays").appendChild(nbsp);
-  }
-  
-  // Padding for dates
-  // use getDay() for first of month to be given a number for the day
-  let padding = new Date(today.getFullYear(), today.getMonth(), 1).getDay()
-  
-  // 0-7 == Sun, Mon, ..., Sat
-  // Padding required is
-  // Mon = 0, Tue = 1, ...., Sun = 6
-  // Logic is: if getDays=0 then use 6 else getDays()-1
-  padding == 0 ? padding = 6 : padding = padding -1
-  
-  // Dates 1 to last day of month
-  for (index=1-padding;   index<32; index++) {
-    let node = document.createElement("li");
-    // let nbsp = document.createTextNode("\u00A0")
-    if (index > 0) {
-      node.appendChild(document.createTextNode(index));
+    let index;
+    const weekDays = document.getElementById("weekdays");
+    weekDays.innerHTML = "";
+    console.log(weekDays.childElementCount);
+    
+    for (index=0;   index<7; index++) {
+      let node = document.createElement("li");
+      // let nbsp = document.createTextNode("\u00A0")
+      node.appendChild(document.createTextNode(shortDays[index]));
+      weekDays.appendChild(node);
+      // document.getElementById("weekdays").appendChild(nbsp);
     }
-    document.getElementById("days").appendChild(node);
-    // document.getElementById("weekdays").appendChild(nbsp);
-  }
+  
+  // days (1-31)
+    // Padding for dates
+    // use getDay() for first of month to be given a number for the day
+    let padding = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
+    // 0-7 == Sun, Mon, ..., Sat
+    // Padding required is
+    // Mon = 0, Tue = 1, ...., Sun = 6
+    // Logic is: if getDays=0 then use 6 else getDays()-1
+    padding == 0 ? padding = 6 : padding = padding -1
+
+    // Number of days in month
+    const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+    
+    const daysObj = document.getElementById("days");
+    daysObj.innerHTML = "";
+    // Dates 1 to last day of month
+    for (index=1-padding;   index<daysInMonth+1; index++) {
+      let node = document.createElement("li");
+      // let nbsp = document.createTextNode("\u00A0")
+      if (index > 0) {
+        node.appendChild(document.createTextNode(index));
+      }
+      daysObj.appendChild(node);
+      // document.getElementById("weekdays").appendChild(nbsp);
+    }
   }
   
--- index.js
+++ index.js
@@ -11,9 +11,9 @@ document.getElementById("prev").addEventListener('click',() => {
   } else {
     workingDate =new Date(workingDate.getFullYear(),workingDate.getMonth()-1,workingDate.getDate())
   }
-  
   drawCalendar(workingDate)
 })
+
 
 document.getElementById("next").addEventListener('click',() => {
   console.log("next function");
@@ -22,6 +22,20 @@ document.getElementById("next").addEventListener('click',() => {
   } else {
     workingDate = new Date(workingDate.getFullYear(),workingDate.getMonth()+1,workingDate.getDate())
   }
+  drawCalendar(workingDate)
+})
+
+// attach event handlers to << and >>
+document.getElementById("prevYear").addEventListener('click',() => {
+  console.log("prevYear function");
+  workingDate = new Date(workingDate.getFullYear()-1,workingDate.getMonth(),workingDate.getDate())
+  drawCalendar(workingDate)
+})
+
+
+document.getElementById("nextYear").addEventListener('click',() => {
+  console.log("nextYear function");
+  workingDate = new Date(workingDate.getFullYear()+1,workingDate.getMonth(),workingDate.getDate())
   drawCalendar(workingDate)
 })
 
